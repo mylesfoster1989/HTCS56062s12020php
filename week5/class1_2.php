61b922a8d49d25c9e8323d79d549d1fe338616cb
unknown <mylesfoster1989@gmail.com>
2020-05-18 10:52:55 +1200

    new

--- week5/class1_2.php
+++ week5/class1_2.php
@@ -9,11 +9,10 @@
 <body>
 <?php
 $i = 1; //set counter
-while ($i <20){ //set condition
-echo "<p>class is over come back tommorrow</p>" ; //do something if condition is true
-$i = $i + 1;
-
-
+while ($i <20) { //set condition
+    echo "<p>class is over come back tommorrow</p>"; //do something if condition is true
+    $i = $i + 1;
+}
 ?>
 </body>
 </html>
