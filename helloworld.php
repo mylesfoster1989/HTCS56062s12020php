59fbc81ccbfe228de892efe01d467facda10c661
unknown <mylesfoster1989@gmail.com>
2020-05-18 10:15:56 +1200

    new

--- helloworld.php
+++ helloworld.php
@@ -2,4 +2,5 @@
 echo "Hello World";
 echo "success";
 
-//https://htcs5606s12020.herokuapp.com/
+
+//https://htcs5606s12020.herokuapp.com/week5/class1.php
--- week5/class1.php
+++ week5/class1.php
@@ -7,3 +7,13 @@ echo "<h1>hello world</h1>";
 echo "<p>hello world</p>";
 
 echo "<p style='color: red'>Hello World</p>";
+
+//https://htcs5606s12020.herokuapp.com/week5/class1.php
+https://github.com/mylesfoster1989/HTCS56062s12020php.git
+
+$i = 0; //set counter
+while ($i <100){ //set condition
+    echo "<p>hello</p>" do something if condition is true
+    $i = $i + 1 //update counter
+
+}
