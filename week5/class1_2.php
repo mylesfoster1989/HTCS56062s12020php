9fcd64a84bfdac9a44c979676f2477b66726c101
unknown <mylesfoster1989@gmail.com>
2020-05-18 10:39:57 +1200

    new

---
+++ week5/class1_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>create php in html</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<?php
+$i = 1; //set counter
+while ($i <20){ //set condition
+echo "<p>class is over come back tommorrow</p>" ; //do something if condition is true
+$i = $i + 1;
+
+
+?>
+</body>
+</html>
