0961e15dbfcb3a795462b54b71d516fdb428c57d
unknown <mylesfoster1989@gmail.com>
2020-05-19 09:56:19 +1200

    new1

---
+++ class2_2.php
@@ -0,0 +1,7 @@
+<?php
+//take the time from system
+//date function to tak ethe time from system or onliune
+$t = data("y-m-d"); //https://www.php.net/manual/en/function.date.php
+
+$t1 = date("H.i.s");
+echo $1. " ".$t1;
