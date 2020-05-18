af44966baafe1f53608edf72cdf868a4f169e07e
unknown <mylesfoster1989@gmail.com>
2020-05-19 09:07:15 +1200

    new1

--- week5/class2.php
+++ week5/class2.php
@@ -7,3 +7,9 @@ echo $grating;
 echo  $who;
 
 echo "<h1>" . $grating . " " . $who . "</h1>"; dont mean merge in php
+
+$x = 4;
+$y = 5;
+echo "x = ".$x;
+echo "y = ".$y;
+echo "z = x + y = ".($x + $y);
