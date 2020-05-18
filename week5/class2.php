871cc68d0fc57d537823f97fffcb6748ce609b12
unknown <mylesfoster1989@gmail.com>
2020-05-19 09:37:09 +1200

    new

--- week5/class2.php
+++ week5/class2.php
@@ -13,3 +13,20 @@ $y = 5;
 echo "x = ".$x;
 echo "y = ".$y;
 echo "z = x + y = ".($x + $y);
+
+$grating = "Hello world"
+//lines of string
+echo "<br>".strlen($grating) "string length </br>"; //stringlength
+//word count -- count numbers of word in the string
+echo str_word_count($grating)."string word count<br>"
+    //string reverse
+echo strrev($grating). "string reverse<br>";
+//string position
+echo strpos($grating, "Hello")."word position<br>"
+//string replace
+echo str_replace($grating,"Hello","byebye")."
+word replace<br>";
+//to uppercase
+echo strtoupper($grating)."To Upper Case <br>";
+//to lower
+echo strtolower($grating). "To Lower Case <br>";
