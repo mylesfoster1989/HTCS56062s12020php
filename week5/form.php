f38a561dd5bf58feff5abc3cf3acdd31839dddf8
unknown <mylesfoster1989@gmail.com>
2020-05-20 09:11:04 +1200

    new

--- week5/form.php
+++ week5/form.php
@@ -4,10 +4,12 @@
 <!--methoid has get aann post, get is your request from server,post is server ask to send data to it and process for you
 method in form is your request method, name inside input box is you request data name-->
 <form action="https://www.unitec.co.nz" method"get">
+<!--post allows encrypt -->
+<!--get no way to encrypt-->
 <p>
     mark: <input type="number" name="mark"
 </p>
 <p>
-    <input type="submit value="show>
+    <input type="submit" value="show>
 </p>
 </form>
--- week5/formresult.php
+++ week5/formresult.php
@@ -6,10 +6,13 @@ if ($res > 100 || $res <0) { //condition
 }elseif(res > 90){
     echo "A";
 
-}elseif(res > 90){
+}elseif(res > 80){
     echo "b";
 
+}elseif(res > 50){
+    echo "c";
 
-
+}elseif(res > 50){
+    echo "d";
 
 }
