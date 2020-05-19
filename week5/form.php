8da041f456b94c5e3820ae67728b47aa9891361d
unknown <mylesfoster1989@gmail.com>
2020-05-19 22:05:27 +1200

    new

--- week5/form.php
+++ week5/form.php
@@ -5,6 +5,21 @@
 <p>
     mark: <input type="number" name="mark"
 <p>Grade: <span id = "showGrade"></span></p>
+<p>pass: <b id="showPass"></b> </p>
+
+<h2>show mark range</h2>
+<p>
+    Grade:<br>
+    <select id="gradeSelect">
+        <option>A</option>
+        <option>B</option>
+        <option>C</option>
+        <option>D</option>
+        <option>E</option>
+        <option>F</option>
+    </select>
+
+
 </p>
 <p>
     <input type="submit" value="show">
