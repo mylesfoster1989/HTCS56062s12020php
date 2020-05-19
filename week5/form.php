b649873c87da5d0366ee73f4bc1d2a08cc3cbdd8
unknown <mylesfoster1989@gmail.com>
2020-05-20 09:01:41 +1200

    new

--- week5/form.php
+++ week5/form.php
@@ -1,27 +1,13 @@
 <?php
 ?>
-
-<form action="https://www.unitec.co.nz" method"post">
+<!--action is the url you send your request-->
+<!--methoid has get aann post, get is your request from server,post is server ask to send data to it and process for you
+method in form is your request method, name inside input box is you request data name-->
+<form action="https://www.unitec.co.nz" method"get">
 <p>
     mark: <input type="number" name="mark"
-<p>Grade: <span id = "showGrade"></span></p>
-<p>pass: <b id="showPass"></b> </p>
-
-<h2>show mark range</h2>
-<p>
-    Grade:<br>
-    <select id="gradeSelect">
-        <option>A</option>
-        <option>B</option>
-        <option>C</option>
-        <option>D</option>
-        <option>E</option>
-        <option>F</option>
-    </select>
-
-
 </p>
 <p>
-    <input type="submit" value="show">
+    <input type="submit value="show>
 </p>
 </form>
--- week5/formresult.php
+++ week5/formresult.php
@@ -1,26 +1,15 @@
 <?php
-$res = $_POST["mark"];
-if ($res > 50){ //condition
-    echo "pass";
+//$res = $_POST["mark"];
+if ($res > 100 || $res <0) { //condition
+    echo "wrong input";
 
-}else {
-        pass = "no";
+}elseif(res > 90){
+    echo "A";
+
+}elseif(res > 90){
+    echo "b";
+
+
+
 
 }
-
-     showGrade = document.getElementById("showGrade")
-     if (mark > 90 && mark <= 100) {
-         grade = "A";
-     }
-     if (mark > 75 && mark <= 90) {
-         grade = "B";
-     }
-     if (mark > 60 && mark <= 75) {
-         grade = "C";
-     }
-     if (mark > 50 && mark <= 60) { // && means
-         grade = "D";
-     }
-     if (mark > 100 || mark < 0) // || means or !means not {
-         grade = "Wrong Input";
- }
