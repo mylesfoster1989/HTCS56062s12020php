6d49ad0f5b7fdd2ed2271e818b56624bc09468de
unknown <mylesfoster1989@gmail.com>
2020-05-20 08:44:13 +1200

    new

--- week5/form.php
+++ week5/form.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 
-<form action="formresult.php" method"post">
+<form action="https://www.unitec.co.nz" method"post">
 <p>
     mark: <input type="number" name="mark"
 <p>Grade: <span id = "showGrade"></span></p>
