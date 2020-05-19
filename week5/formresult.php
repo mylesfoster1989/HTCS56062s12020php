b1500d867688b4a303803757f533732e1f895072
unknown <mylesfoster1989@gmail.com>
2020-05-20 09:17:31 +1200

    new

--- week5/formresult.php
+++ week5/formresult.php
@@ -3,16 +3,16 @@ $res = $_POST["mark"];
 if ($res > 100 || $res <0) { //condition
     echo "wrong input";
 
-}elseif(res > 90){
+}elseif($res > 90){
     echo "A";
 
-}elseif(res > 80){
+}elseif($res > 80){
     echo "b";
 
-}elseif(res > 50){
+}elseif($res > 50){
     echo "c";
 
-}elseif(res > 50){
+}elseif($res > 50){
     echo "d";
 
 }
