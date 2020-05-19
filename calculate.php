1beee70ae09c5f200fd07e0585b9a70dc97e4d93
unknown <mylesfoster1989@gmail.com>
2020-05-20 09:32:43 +1200

    new

---
+++ calculate.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST["width"])){ //checks post data
+    $w = $_POST["width"];
+$h = $_POST["height"];
+$area = $w * $h;
+echo "area: "/$area;
+
+
+
+}else{
+    echo "go back to page";
+}
---
+++ week5/form2.php
@@ -0,0 +1,9 @@
+<?php
+?>
+
+<form action="calculate" method="post">
+    <p>Calculate area rectangle</p>
+    <p>Width: <input name ="width" type="text"></p>
+    <p>Height: <input name ="height" type="text"></p>
+    <p><input value ="submit" type="submit"</p>
+</form>
