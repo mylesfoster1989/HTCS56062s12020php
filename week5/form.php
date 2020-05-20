c794a303463eaf721cf2c147f3338ac41147d221
unknown <mylesfoster1989@gmail.com>
2020-05-21 11:23:28 +1200

    helloow

--- week5/form.php
+++ week5/form.php
@@ -3,7 +3,7 @@
 <!--action is the url you send your request-->
 <!--methoid has get aann post, get is your request from server,post is server ask to send data to it and process for you
 method in form is your request method, name inside input box is you request data name-->
-<form action="formresult.php" method="post"> <!--can do gee instead of post-->
+<form action="formresult.php" method="post"> <!--can do gee instead of post-->gi
 <!--post allows encrypt -->
 <!--get no way to encrypt-->
 <p>
