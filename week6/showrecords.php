0ea3a52a229c27e30f895b9d797088cb10f44108
unknown <mylesfoster1989@gmail.com>
2020-05-25 10:03:31 +1200

    new

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -4,9 +4,21 @@ $user =  "jo0xc9likdk5o0vx";
 $pwd = "nvvui8agli8r5q6e";
 $database = "bhdh4cks7ou9mirv";
 
-$conn = new mysqli($server,$user,$pwd,$database);
+$conn = new mysqli($server,$user,$pwd,$database); //create database connection
 if ($conn->connect_error){
     echo $conn->connect_error;
 }else{
     echo "Connection Created";
 }
+
+$sql = "select * from Users";
+$result = $conn->query ($sql); //run the query on this connection
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row['id'] . " " . $row['username'] . " " . $row['name'] . "<br>";//in each row we have column
+    }
+}else{
+    echo  "no result in table";
+}
+$conn->close();
