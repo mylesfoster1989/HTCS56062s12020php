3d4c14ff1fa8cdc978c2c171ff7d1e74f97b60c1
unknown <mylesfoster1989@gmail.com>
2020-05-25 09:53:15 +1200

    week6

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -1,8 +1,8 @@
 <?php
-$server =  " arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$user =  " jo0xc9likdk5o0vx";
-$pwd = " nvvui8agli8r5q6e";
-$database ="bhdh4cks7ou9mirv";
+$server =  "arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$user =  "jo0xc9likdk5o0vx";
+$pwd = "nvvui8agli8r5q6e";
+$database = "bhdh4cks7ou9mirv";
 
 $conn = new mysqli($server,$user,$pwd,$database);
 if ($conn->connect_error){
