65d3af221a44472a2e2f5dce48ab7073151ddbb6
unknown <mylesfoster1989@gmail.com>
2020-05-28 09:52:34 +1200

    new

--- week6/login.php
+++ week6/login.php
@@ -46,7 +46,7 @@ if (isset($_POST["username"])){
             ?>
     <script>
         setTimeout(function () {
-            window.open("loginform.html", "_self"); // go to login form
+            window.open("loginform.php", "_self"); // go to login form
         }, 3000);
 
     </script>
