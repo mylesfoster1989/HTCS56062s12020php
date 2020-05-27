17162f0ad5c9ec2bdc70dc97f4f562f6f2b92210
unknown <mylesfoster1989@gmail.com>
2020-05-28 09:29:09 +1200

    new

--- week6/loginform.php
+++ week6/loginform.php
@@ -5,11 +5,12 @@
     <title>Login Form</title>
 </head>
 <body>
-<?
+<?php
+include_once "login.php";
 session_start();
 if (isset($_session["username"])){ //IF LOGIN, ELSE SHOW FORM
-
     ?>
+
     <p><a href="profile.php">profile</a></p>
     <p><a href="changepasswordform.php">change password</a></p>
     <p><a href="logout.php">Logout</a></p>
@@ -20,7 +21,7 @@ if (isset($_session["username"])){ //IF LOGIN, ELSE SHOW FORM
 ?>
 
 <!--HTML Code between php block-->
-<form action="login.php" method="post">
+<form action="<?php echo $SERVER['PHP_SELF']; //POPST REQUEST TO SAME PAGE ?>" method="post">
     <p>Username: <input type="text" name="username" required></p>
     <p>Password: <input type="password" name="password" required></p>
     <p><button>Login</button></p> <!--Exact same to input type submit-->
