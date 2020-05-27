12446017bb8a4066bd9e745b1d491b6419bec7e5
unknown <mylesfoster1989@gmail.com>
2020-05-28 10:04:45 +1200

    new

--- week6/logout.php
+++ week6/logout.php
@@ -3,3 +3,11 @@
 
 session_start();
 session_destroy(); //remove everything in session
+
+?>
+<script>
+    setTimeout(function () {
+        window.open("loginform.php", "_self"); // go to login form
+    }, 3000);
+
+</script>
