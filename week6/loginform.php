0a01ae69440daa3bab174e1d41395d343552223e
unknown <mylesfoster1989@gmail.com>
2020-05-28 09:54:55 +1200

    new

--- week6/loginform.php
+++ week6/loginform.php
@@ -17,11 +17,11 @@ if (isset($_session["username"])){ //IF LOGIN, ELSE SHOW FORM
 
     <?php
 
-}else{}
+}else{
 ?>
 
 <!--HTML Code between php block-->
-<form action="<?php echo $SERVER['PHP_SELF']; //POPST REQUEST TO SAME PAGE ?>" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']; //POPST REQUEST TO SAME PAGE ?>" method="post">
     <p>Username: <input type="text" name="username" required></p>
     <p>Password: <input type="password" name="password" required></p>
     <p><button>Login</button></p> <!--Exact same to input type submit-->
