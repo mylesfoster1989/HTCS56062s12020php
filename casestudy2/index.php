cc1c41129232946ed35b7047d4615ad3c4f3fd78
unknown <mylesfoster1989@gmail.com>
2020-05-29 17:29:09 +1200

    new

---
+++ casestudy2/index.php
@@ -0,0 +1,62 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>homePage</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/script.js"></script>
+</head>
+
+<body onload="leftDividerAnimation()">
+
+<?php
+include_once "header.php";
+?>
+
+
+<h1>
+
+    <div id="navigation1"> <!-- nav i will create my menu here-->
+        <ul> <!--use to create menu-->
+            <li><a href="index.php">Home
+            </a></li>
+            <li><a href="Product.html">Products
+            </a></li>
+            <li><a href="Career.html">Career
+            </a></li>
+            <li><a href="aboutus.php">About Us
+            </a></li>
+        </ul>
+    </div>
+</h1>
+
+<hr>
+
+<div id="navigation"> <!-- nav i will create my menu here-->
+    <ul> <!--use to create menu-->
+        <li><a href="Dog.html">
+
+            <img src="image/dog.jpg" height="239" width="210"/></a></li>
+        <li><a href="Cat.html"><img src="image/cat.jpg" height="228" width="221"/>
+        </a></li>
+        <li><a href="Fish.html"><img src="image/fish.png" height="165" width="305"/>
+        </a></li>
+        <li><a href="Reptile.html"><img src="image/reptile.png" height="188" width="268"/>
+        </a></li>
+        <li><a href="SmallAnimal.html"><img src="image/small animals.jpg" height="174" width="290"/>
+        </a></li>
+        <li><a href="Chicken.html"><img src="image/chicken.png" height="189" width="267"/>
+        </a></li>
+        <li><a href="#"><img src="image/bird.png" height="204" width="247"/>
+        </a></li>
+    </ul>
+
+
+        <?php
+        include_once "footer.php";
+        ?>
+
+</div>
+</body>
+
+
+</html>
