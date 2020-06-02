08bab934da382c953e71716cff8df0c71abf7b49
unknown <mylesfoster1989@gmail.com>
2020-06-03 10:01:49 +1200

    new

--- week6/databaseconnection.php
+++ week6/databaseconnection.php
@@ -107,14 +107,14 @@ function showRecords()
 {
     $conn = dbconn();
     $sql = "select * from Users"; // create query
-    $result = $connection->query($sql); //run the query on this connection
-    $records - array();
+    $result = $conn->query($sql); //run the query on this connection
+    $records = array();
 
     if ($result->num_rows > 0) { //check if there is record in the result
         while ($row = $result->fetch_assoc()) { //show each associated row
             echo $row['id'] . " " . $row['username'] . " " . $row['password'] . " " . $row['name'] . "<br>"; // in each row, we have columns.
-            $records = array($row['id'], $row['username'], $row['password'], $row['name']);
-            array_push(, $records, $record);
+            $record = array($row['id'], $row['username'], $row['password'], $row['name']);
+            array_push( $records, $record);
         }
 
     } else {
