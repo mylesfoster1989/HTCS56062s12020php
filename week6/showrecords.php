854b2510dcd7dca9bb4aac09e3a20fe007336f1c
unknown <mylesfoster1989@gmail.com>
2020-06-03 10:14:13 +1200

    new

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -2,10 +2,16 @@
 
 include_once "databaseconnection.php";
 $records = showRecords();
-print_r($records);
+//print_r($records);
 
-$i = 0
+$i = 0;
     while ($i < sizeOf($records)){
-    print_r($records[i]);
+    print_r($records[$i]); //arrayname index is the value --. value
+    $record = $records[$i];
+    ?>
+        <p><?php echo $records[0]; ?> <?php echo $record[1]; ?> <?php echo $record[2]; ?> <?php echo $records[3]; ?></p>
+
+<?php
+        $i = $i + 1;
     }
 
