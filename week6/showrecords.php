f1e128c6788fb2df2a784af3044d0966f2654998
unknown <mylesfoster1989@gmail.com>
2020-06-04 09:29:54 +1200

    new

--- week6/showrecords.php
+++ week6/showrecords.php
@@ -9,9 +9,9 @@ $i = 0;
     //print_r($records[$i]); //arrayname index is the value --. value
     $record = $records[$i]; // inside each record we have aplace 0 for id,place 1 for username, place 2 for password
     ?>
-        <p><?php echo $record->id;
-        ?> <?php echo $record->username;
-        ?> <?php echo $record->password;
+        <p><?php echo $record->id; ?>
+        ?> <?php //echo $record->username; ?>
+        ?> <?php //echo $record->password; ?>
         ?> <?php echo $record->name; ?>
         </p>
 
