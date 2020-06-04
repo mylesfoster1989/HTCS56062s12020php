697da98a4679e33d55f129887ec2d29354742c99
unknown <mylesfoster1989@gmail.com>
2020-06-04 15:58:21 +1200

    new

--- casestudy2/class/DB.php
+++ casestudy2/class/DB.php
@@ -3,11 +3,11 @@
 
 class DB
 {
-private $server = "arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    private $server = "arfo8ynm6olw6vpn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     private $user = "jo0xc9likdk5o0vx";
     private $pwd = "nvvui8agli8r5q6e";
     private $database = "bhdh4cks7ou9mirv";
-public $connectoion;
+    public $connection;
 
     /**
      * DB constructor.
@@ -15,9 +15,9 @@ public $connectoion;
     public function __construct()
     {
         $this->connection = new mysqli($this->server, $this->user, $this->pwd, $this->database); //this --> his
-       if ($this->connection->connect_error){
-           echo $this->connection->connect_error;
-       }
+        if ($this->connection->connect_error) {
+            echo $this->connection->connect_error;
         }
+    }
 
 }
