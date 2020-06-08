28bd86f3a70ceab25d8cb3dc0dd24cf092fc7410
unknown <mylesfoster1989@gmail.com>
2020-06-09 09:51:20 +1200

    userandcat

--- casestudy2/header.php
+++ casestudy2/header.php
@@ -5,14 +5,17 @@
 
     <div id="navigation1"> <!-- nav i will create my menu here-->
         <ul> <!--use to create menu-->
-            <li><a href="index.php">Home
-                </a></li>
-            <li><a href="Product.html">Products
-                </a></li>
-            <li><a href="Career.html">Career
-                </a></li>
-            <li><a href="aboutus.php">About Us
-                </a></li>
+            <li><a href="index.php">Home</a></li>
+  <!--          <li><a href="Product.html">Products</a></li>-->
+ <!--           <li><a href="Career.html">Career</a></li>-->
+
+ <!--           <li><a href="aboutus.php">About Us</a></li>-->
+            <?php
+            include_once "class/User.php";
+            $user = new User(null, "","","");
+            $categories = $user->viewCategories();
+            print_r($categories);
+            ?>
         </ul>
     </div>
 </h1>
