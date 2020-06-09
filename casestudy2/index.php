287041db8c7c8e3445d84a5a097e60611b3f1678
unknown <mylesfoster1989@gmail.com>
2020-06-10 09:31:23 +1200

    new2

--- casestudy2/index.php
+++ casestudy2/index.php
@@ -3,6 +3,10 @@
     <meta charset="UTF-8">
     <title>homePage</title>
     <link rel="stylesheet" href="css/style.css">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
 </head>
 
--- casestudy2/products.php
+++ casestudy2/products.php
@@ -2,8 +2,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Product</title>
-    <script src="js/script.js"></script>
     <link rel="stylesheet" href="css/style.css">
+    <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossorigin="anonymous"></script>
+    <script src="js/script.js"></script>
+
+
 
 </head>
 <body onload="leftDividerAnimation(), invoice()">
@@ -31,8 +37,11 @@ $product = $products[$i];
 <div class="product">
     <img src="<?php echo $product->imageName;?>" height="257" width="196"/>
     <p>price: <?php echo $product->price;?></p>
-    <p>qty:<input class="qty" id="catQty" type="number">
-        <button class="<?php echo $product->name;?>" id="addCat" onclick="addToCart(this)">Add to Cart</button>
+    <p>
+       <input placeholder="qty" id="catQty" type="number">
+        <button name="<?php echo $product->name;?>
+        price = <?php echo $product->price; ?>
+                onclick="addToCart(this)">Add to Cart</button>
     </p>
 </div>
 
